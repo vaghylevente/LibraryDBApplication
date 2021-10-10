3738d9972e33ae902268baa8df09655dd07b9fba
vaghylevente <vaghylevente@gmail.com>
2021-10-10 16:09:57 +0200

    program structure

---
+++ FDU7VL_HFT_2021221/FDU7VL_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FDU7VL_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
