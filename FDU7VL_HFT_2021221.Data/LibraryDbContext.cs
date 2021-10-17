0143aae03ebd0919b2679387877f1466a1e0bb3a
vaghylevente <vaghylevente@gmail.com>
2021-10-17 20:26:33 +0200

    DbContext class added

---
+++ FDU7VL_HFT_2021221.Data/LibraryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FDU7VL_HFT_2021221.Models;
+
+namespace FDU7VL_HFT_2021221.Data
+{
+    class LibraryDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security = True");
+            }
+        }
+
+    }
+}
