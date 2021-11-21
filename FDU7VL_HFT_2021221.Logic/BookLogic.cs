5f4e227a78ac06b35e0b6b82c6e12b3c4eb9dcfb
vaghylevente <vaghylevente@gmail.com>
2021-11-21 13:14:04 +0100

    BookLogic and its interface created with CRUD methods

--- FDU7VL_HFT_2021221.Logic/BookLogic.cs
+++ FDU7VL_HFT_2021221.Logic/BookLogic.cs
@@ -1,4 +1,5 @@
-﻿using FDU7VL_HFT_2021221.Repository;
+﻿using FDU7VL_HFT_2021221.Models;
+using FDU7VL_HFT_2021221.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,48 @@ using System.Threading.Tasks;
 
 namespace FDU7VL_HFT_2021221.Logic
 {
-    class BookLogic
+    public class BookLogic : IBookLogic
     {
+        IBookRepository repo;
+        public BookLogic(IBookRepository bookRepository)
+        {
+            repo = bookRepository;
+        }
+        public void Create(Book book)
+        {
+            if (book.Title == null || book.Title == "")
+            {
+                throw new ArgumentException("The book has to have a title!");
+            }
+            if (book.Author == null || book.Author == "")
+            {
+                book.Author = "unknown";
+                repo.Create(book);
+            }
+            else
+            {
+                repo.Create(book);
+            }
+        }
 
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Book Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Book> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Book book)
+        {
+            repo.Update(book);
+        }
     }
 }
--- FDU7VL_HFT_2021221.Logic/IBookLogic.cs
+++ FDU7VL_HFT_2021221.Logic/IBookLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FDU7VL_HFT_2021221.Models;
+using FDU7VL_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace FDU7VL_HFT_2021221.Logic
 {
     public interface IBookLogic
     {
-
+        void Create(Book book);
+        Book Read(int id);
+        IQueryable<Book> ReadAll();
+        void Update(Book book);
+        void Delete(int id);
     }
 }
