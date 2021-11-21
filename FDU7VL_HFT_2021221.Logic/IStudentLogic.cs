cabe967303298df85bf99898aa9118ab8f0f26a5
vaghylevente <vaghylevente@gmail.com>
2021-11-21 13:26:04 +0100

    StudentLogic and its interface created with CRUD methods

--- FDU7VL_HFT_2021221.Logic/IStudentLogic.cs
+++ FDU7VL_HFT_2021221.Logic/IStudentLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FDU7VL_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace FDU7VL_HFT_2021221.Logic
 {
-    interface IStudentLogic
+    public interface IStudentLogic
     {
+        void Create(Student student);
+        Student Read(int id);
+        IQueryable<Student> ReadAll();
+        void Update(Student student);
+        void Delete(int id);
     }
 }
--- FDU7VL_HFT_2021221.Logic/StudentLogic.cs
+++ FDU7VL_HFT_2021221.Logic/StudentLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FDU7VL_HFT_2021221.Models;
+using FDU7VL_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace FDU7VL_HFT_2021221.Logic
 {
-    class StudentLogic
+    public class StudentLogic : IStudentLogic
     {
+        IStudentRepository repo;
+        public StudentLogic(IStudentRepository studentRepository)
+        {
+            repo = studentRepository;
+        }
+        public void Create(Student student)
+        {
+            if (student.Name == null || student.Name == "")
+            {
+                throw new ArgumentException("The student has to have a name!");
+            }
+            else
+            {
+                repo.Create(student);
+            }
+        }
+        
+        public Student Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Student> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Student student)
+        {
+            repo.Update(student);
+        }
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
     }
 }
