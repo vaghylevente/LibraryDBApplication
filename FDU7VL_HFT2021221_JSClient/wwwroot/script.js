6eee964c02ea55cf7d2b64f8d6309df6b135b6c0
vaghylevente <vaghylevente@stud.uni-obuda.hu>
2022-05-08 14:57:20 +0200

    Basic table of students added

--- FDU7VL_HFT2021221_JSClient/wwwroot/script.js
+++ FDU7VL_HFT2021221_JSClient/wwwroot/script.js
@@ -1,3 +1,28 @@
-﻿fetch('http://localhost:20621/student')
+﻿let students = [];
+
+fetch('http://localhost:20621/student')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        students = y;
+        console.log(students);
+        display();
+    });
+
+function display() {
+    students.forEach(t => {
+        /*document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.studentID + "</td><td>" + t.name + "</td><td>" + t.class + "</td><tr>";
+        /*document.getElementById('resultarea').innerHTML +=
+            "<tr>" + 
+            "<td>" + t.studentID + "</td>" +
+            "<td>" + t.name + "</td>" +
+            "<td>" + t.class + "</td>" +
+            "</tr> ";*/
+        document.getElementById('resultarea').innerHTML += `
+            <tr>
+            <td>${t.studentID}</td>
+            <td>${t.name}</td>
+            <td>${t.class}</td>
+            </tr>`
+    })
+}
